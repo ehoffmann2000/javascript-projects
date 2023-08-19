dbe7ed8466faeb2b98ba4f15a6afd39160dbf396
ehoffmann2000 <ethan.hoffmann2000@gmail.com>
2023-08-19 16:46:09 -0500

    changed console log to test commit

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -4,3 +4,6 @@ console.log("What","do","commas","do?");
 console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);
+
+//added this to test commit
+console.log("writing","\tpencil");
